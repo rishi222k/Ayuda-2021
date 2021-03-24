b6af586c14c28e873dc4b43d941cbf49637f64af
rishi222k <rishiraghu22@yahoo.com>
2021-03-24 13:55:48 +0530

    Responsive Landing

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -10,17 +10,17 @@ const Landing = () => {
     return (
         <div className="Landing">
         <div className="row">
-        <div className="col-6">
+        <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
         <Nav/>
         <div className=" titles caption1">
             <hr/>
             <h4>WE ARE AYUDA NGO</h4>
         </div>
         <div className=" titles caption2">
-        <p>At <span>Ayuda</span>, we work towards <br/>the betterment of our <span>world</span> </p>
+        <p>At <span>Ayuda</span>, we work towards the betterment of our <span>world</span> </p>
         </div>
         <div className=" titles caption3">
-        <p>Bring out the change in society by <br/> spreading hands to help the unprivileged <br/> children and other backward communities.</p>
+        <p>Bring out the change in society by spreading hands to help the unprivileged children and other backward communities.</p>
         </div>
         <div className=" titles btns">
             <a href="." className="Learn">Learn More</a>
@@ -28,27 +28,31 @@ const Landing = () => {
         </div>
         
         </div>
-        <div className="col-6">
-            <img src={Main} alt="Mainimg"/>
+        <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+            <img src={Main} alt="Mainimg" id="Mainimg"/>
             <div className="social">
             <FacebookBox
                 size="30"
                 color="#a9a9a9"
+                className="icon"
             />
             <br/>
             <Instagram
                 size="30"
                 color="#a9a9a9"
+                className="icon"
             />
             <br/>
             <Linkedin 
                 size="30"
                 color="#a9a9a9"
+                className="icon"
             />
             <br/>
             <Medium 
                 size="30"
                 color="#a9a9a9"
+                className="icon"
             />
             </div>
         </div>
