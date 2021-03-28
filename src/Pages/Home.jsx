c889c247023b055960e56e73de13bd6d558c7477
rishi222k <rishiraghu22@yahoo.com>
2021-03-29 02:26:56 +0530

    fixes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import "../../node_modules/bootstrap/dist/css/bootstrap-grid.css"
 import Blogs from '../Components/Blogs'
-import Events from '../Components/Events'
-import Initiatives from '../Components/Initiatives'
+// import Events from '../Components/Events'
+// import Initiatives from '../Components/Initiatives'
 import Landing from '../Components/Landing'
 import Story from '../Components/Story'
 import '../Styles/Home.scss'
