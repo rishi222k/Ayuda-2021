b6814dd720b26de9b4fb33c2d64c6dc84e0ae516
rishi222k <rishiraghu22@yahoo.com>
2021-03-29 13:26:14 +0530

    Blogspot

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -17,27 +17,27 @@ const Blogs = () => {
             </div>
             <div className="card">
                 <h2>THE <span> CHILDHOOD </span>DREAM</h2>
-                <p>Back in my school days, we had an annual visit to the old age home and an orphanage once a year at least. We’d go there and spend time with them, distribute things they might need or like and plan activities for them... </p>
+                <p>Back in my school days, we had an annual visit to the old age home and an orphanage once a year at least. We’d go there and spend time with them, distribute things they might need or like and plan activities for them ... </p>
                 <a href=".">Learn More</a>
             </div>
             <div className="card">
                 <h2>THE PREDICAMENT OF <span> RISING POPULATION</span></h2>
-                <p>Every year, on July 11, World Population Day is observed by the UN with the primary narrative being to promulgate awareness on the challenges faced due to the incrementing...</p>    
+                <p>Every year, on July 11, World Population Day is observed by the UN with the primary narrative being to promulgate awareness on the challenges faced due to the ...</p>    
                 <a href=".">Learn More</a>
             </div>
             <div className="card">
                 <h2><span> DON’T</span> GIVE UP - You are <span> important</span> as your <span>Life </span></h2>
-                <p>Every hour one student commits suicide in India, with about 28 such suicides reported every day, according to data compiled...</p>
+                <p>Every hour one student commits suicide in India, with about 28 such suicides reported every day, according to data compiled ...</p>
                 <a href=".">Learn More</a>
             </div>
             <div className="card">
                 <h2>MY JOURNEY IN <span>AYUDA</span></h2>
-                <p>When I look back at the two and a half years I have spent in this club, I see so many memories flooding my mind. I see the three Rivieras where we put our heart and souls to sell stuff, I see the countless number...</p>
+                <p>When I look back at the two and a half years I have spent in this club, I see so many memories flooding my mind. I see the three Rivieras where we put our heart and souls to sell stuff, I see the countless number ...</p>
                 <a href=".">Learn More</a>
             </div>
             <div className="card">
                 <h2>DRUG <span> ABUSE </span>AND ILLICIT <span>TRAFFICKING</span></h2>
-                <p>International Day Against Drug Abuse and Illicit Trafficking is a United Nations International Day against drug abuse and the illegal drug trade. It is observed annually on...</p>
+                <p>International Day Against Drug Abuse and Illicit Trafficking is a United Nations International Day against drug abuse and the illegal drug trade. It is observed annually on ...</p>
                 <a href=".">Learn More</a>
             </div>
             
