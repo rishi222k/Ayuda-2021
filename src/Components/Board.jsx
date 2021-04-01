5ba58102b7df0ca574f7434b3de5a3ba2b7ad099
rishi222k <rishiraghu22@yahoo.com>
2021-04-01 12:35:56 +0530

    Board

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,33 +1,38 @@
 import React from 'react'
-// import Data from '../Data'
-import pic20201 from '../Images/pic2020-1.jpg'
+import Data from '../Data'
 import {Instagram} from '@styled-icons/boxicons-logos/Instagram'
 import { Linkedin } from '@styled-icons/boxicons-logos'
 
 const Board = () => {
-    // const data1 =()=>{
-    //     return(Data[0].y2020)
-    // }
+    const data1 =()=>{
+        return(Data[0].y2021)
+    }
     return (
         <div className="Board">
         <br/>
         <br/>
-        <h1>BOARD MEMBERS</h1>
-        <div className="container">
+        <h1>BOARD <span> MEMBERS</span></h1>
+        <div className="cards">
+        {data1().map((data)=>
         <div className="Card">
-        <img class="Card-top" src={pic20201} alt="Card cap" />
+        <img class="Card-top" src={data.img} alt="Card cap" />
         <div class="card-body">
             <div className="header">
-            <h5 class="Card-title">Vandana Mansur</h5>
-            <div className="social-i">
-            <a href="."><Instagram size="25" color="#ffffff"/></a>
-            <a href="."><Linkedin size="24" color="#ffffff"/> </a>
+            <h5 class="Card-title">{data.name}</h5>
+                <div className="social-i">
+                <a href={data.inst}><Instagram size="25" color="#ffffff"/></a>
+                <a href={data.link}><Linkedin size="24" color="#ffffff"/></a>
+                </div>
             </div>
-            </div>
-            <p class="Card-text">Chairperson</p>
+            <p class="Card-text">{data.desg}</p>
         </div>
+        </div> )}
         </div>
+        <div className="row">
+        <a href="." id="See">See More</a>
         </div>
+        <br/><br/>
+        
         
         </div>
     )
--- src/Data.js
+++ src/Data.js
@@ -1,3 +1,25 @@
+import pic20201 from './Images/Boards/pic2020-1.jpg'
+import pic20202 from './Images/Boards/pic2020-2.jpg'
+import pic20203 from './Images/Boards/pic2020-3.jpg'
+import pic20204 from './Images/Boards/pic2020-4.jpg'
+import pic20205 from './Images/Boards/pic2020-5.jpg'
+import pic20206 from './Images/Boards/pic2020-6.jpg'
+import pic20207 from './Images/Boards/pic2020-7.jpg'
+import pic20208 from './Images/Boards/pic2020-8.jpg'
+import pic20209 from './Images/Boards/pic2020-9.jpg'
+import pic202010 from './Images/Boards/pic2020-10.jpg'
+import pic202011 from './Images/Boards/pic2020-11.jpg'
+import pic20211 from './Images/Boards/pic2021-1.jpg'
+import pic20212 from './Images/Boards/pic2021-2.jpg'
+import pic20213 from './Images/Boards/pic2021-3.jpg'
+import pic20214 from './Images/Boards/pic2021-4.jpg'
+import pic20215 from './Images/Boards/pic2021-5.jpg'
+import pic20216 from './Images/Boards/pic2021-6.jpg'
+import pic20217 from './Images/Boards/pic2021-7.jpg'
+import pic20218 from './Images/Boards/pic2021-8.jpg'
+import pic20219 from './Images/Boards/pic2021-9.png'
+import pic202110 from './Images/Boards/pic2021-10.jpg'
+
 
 const data=
 
@@ -6,34 +28,154 @@ const data=
             y2021:[
 
             { id: "1",
-            // img :,
-            name: "Dhruvil Panchigar",
-            desg: "Captain",},
+            img : pic20211,
+            name: "Swethaa R ",
+            desg: "Chairperson",
+            inst:".",
+            link:"."},
 
             { id: "2",
-            // img :,
-            name: "Anurag Dhudhwani",
-            desg: "Vice - Captain",},
+            img : pic20212,
+            name: "Jainam Desai ",
+            desg: "Vice Chairperson",
+            inst:".",
+            link:"."},
 
             { id: "3",
-            // img :,
-            name: "Mallampali Yashwanth",
-            desg: "Vice - Captain",},
+            img : pic20213,
+            name: "Oishani Sengupta",
+            desg: "Publicity & Marketing Head",
+            inst:".",
+            link:"."},
 
             { id: "4",
-            // img :,
-            name: "Krishnam Kumar",
-            desg: "Manufacturing Head",},
+            img : pic20214,
+            name: "Sakshi Mahajan",
+            desg: "Editorial Head",
+            inst:".",
+            link:"."},
 
             { id: "5",
-            img : "",
-            name: "Zil Chaudhary",
-            desg: "Management Head",},
+            img : pic20215,
+            name: "Deepanshu Srivastava",
+            desg: "Design Head",
+            inst:".",
+            link:"."},
 
             { id: "6",
-            img : "",
-            name: "Bindu Shree V Shetty",
-            desg: "Avionics Head",},
+            img : pic20216,
+            name: "Hartej Singh",
+            desg: "Finance Head",
+            inst:".",
+            link:"."},
+
+            { id: "7",
+            img : pic20217,
+            name: "Aruldharshan.S",
+            desg: "External Events Head",
+            inst:".",
+            link:"."},
+
+            { id: "8",
+            img : pic20218,
+            name: "Shyam R",
+            desg: "External Events Head",
+            inst:".",
+            link:"."},
+
+            { id: "9",
+            img : pic20219,
+            name: "Niketha Sabesan",
+            desg: "Internal Events Head",
+            inst:".",
+            link:"."},
+
+            { id: "10",
+            img : pic202110,
+            name: "Aarushi Bahri",
+            desg: "Internal Events Head",
+            inst:".",
+            link:"."},
+        ]},
+        { key:2,
+            y2020:[
+
+            { id: "1",
+            img : pic20201,
+            name: "Swethaa R ",
+            desg: "Chairperson",
+            inst:".",
+            link:"."},
+
+            { id: "2",
+            img : pic20202,
+            name: "Jainam Desai ",
+            desg: "Vice Chairperson",
+            inst:".",
+            link:"."},
+
+            { id: "3",
+            img : pic20203,
+            name: "Oishani Sengupta",
+            desg: "Publicity & Marketing Head",
+            inst:".",
+            link:"."},
+
+            { id: "4",
+            img : pic20204,
+            name: "Sakshi Mahajan",
+            desg: "Editorial Head",
+            inst:".",
+            link:"."},
+
+            { id: "5",
+            img : pic20205,
+            name: "Deepanshu Srivastava",
+            desg: "Design Head",
+            inst:".",
+            link:"."},
+
+            { id: "6",
+            img : pic20206,
+            name: "Hartej Singh",
+            desg: "Finance Head",
+            inst:".",
+            link:"."},
+
+            { id: "7",
+            img : pic20207,
+            name: "Aruldharshan.S",
+            desg: "External Events Head",
+            inst:".",
+            link:"."},
+
+            { id: "8",
+            img : pic20208,
+            name: "Shyam R",
+            desg: "External Events Head",
+            inst:".",
+            link:"."},
+
+            { id: "9",
+            img : pic20209,
+            name: "Niketha Sabesan",
+            desg: "Internal Events Head",
+            inst:".",
+            link:"."},
+
+            { id: "10",
+            img : pic202010,
+            name: "Aarushi Bahri",
+            desg: "Internal Events Head",
+            inst:".",
+            link:"."},
+
+            { id: "10",
+            img : pic202011,
+            name: "Aarushi Bahri",
+            desg: "Internal Events Head",
+            inst:".",
+            link:"."},
         ]},
     ]
 
