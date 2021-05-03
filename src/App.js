6cf6dd4fe3b24d70cd808b20f06ed41b74eb42c6
rishi222k <rishiraghu22@yahoo.com>
2021-05-03 17:01:33 +0530

    Board Page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import Home from "./Pages/Home";
 import {Switch,Route } from 'react-router-dom'
 import BoardP from "./Pages/BoardP";
 import GalleryP from "./Pages/GalleryP";
+import ScrollTop from './ScrollTop'
 
 function App() {
   return (
     <div className="App">
+    <ScrollTop/>
+
     <Switch>
       <Route path="/" exact>
         <Home/>
--- src/Pages/BoardP.jsx
+++ src/Pages/BoardP.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import logo from '../Images/Logohead.png'
 import '../Styles/BoardPage.scss'
 import {Link} from 'react-router-dom'
+import Data from '../BoardData'
+import {Instagram} from '@styled-icons/boxicons-logos/Instagram'
+import { Linkedin } from '@styled-icons/boxicons-logos'
 
 const BoardP = () => {
+    const data1 =()=>{
+        return(Data[0].y2021)
+    }
     return (
         <div className="BoardP">
              <div className="Bhead row">
-                <div className="col-6 contoverflow">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 contoverflow">
                     <div className="title">
                         <img src={logo} alt="ayudalogo"/>
                         <p>Ayuda</p>
@@ -19,10 +25,71 @@ const BoardP = () => {
                     <Link to="/">View Here</Link>
                     </div>
                 </div>
-                <div className="col-1"></div>
-                <div className="col-5 boardscroll">
+                <div className="col-0 col-sm-0 col-md-0 col-lg-1 col-xl-1"></div>
+                <div className="col-0 col-sm-0 col-md-0 col-lg-5 col-xl-5 boardscroll">
                     <div className="roll"></div>
                 </div>
+            </div>
+            <br/><br/><br/><br/><br/><br/>
+            <div className="Bcards">
+            <h1>2021-2022</h1>
+            <div className="cards">
+            {data1().map((data)=>
+                <div className="Card" key={data.id}>
+                <img className="Card-top" src={data.img} alt="Card cap" />
+                <div className="card-body">
+                    <div className="header">
+                    <h5 className="Card-title">{data.name}</h5>
+                        <div className="social-i">
+                        <a href={data.inst}><Instagram size="25" color="#ffffff"/></a>
+                        <a href={data.link} id="linkedin"><Linkedin size="24" color="#ffffff"/></a>
+                        </div>
+                    </div> 
+                    <p className="Card-text">{data.desg}</p>
+                </div>
+                </div> )}  
+            </div>
+
+            <br/><br/>
+
+            <h1>2020-2021</h1>
+            <div className="cards">
+            {data1().map((data)=>
+                <div className="Card" key={data.id}>
+                <img className="Card-top" src={data.img} alt="Card cap" />
+                <div className="card-body">
+                    <div className="header">
+                    <h5 className="Card-title">{data.name}</h5>
+                        <div className="social-i">
+                        <a href={data.inst}><Instagram size="25" color="#ffffff"/></a>
+                        <a href={data.link} id="linkedin"><Linkedin size="24" color="#ffffff"/></a>
+                        </div>
+                    </div> 
+                    <p className="Card-text">{data.desg}</p>
+                </div>
+                </div> )}  
+            </div>
+            
+            <br/><br/>
+
+            <h1>2019-2020</h1>
+            <div className="cards">
+            {data1().map((data)=>
+                <div className="Card" key={data.id}>
+                <img className="Card-top" src={data.img} alt="Card cap" />
+                <div className="card-body">
+                    <div className="header">
+                    <h5 className="Card-title">{data.name}</h5>
+                        <div className="social-i">
+                        <a href={data.inst}><Instagram size="25" color="#ffffff"/></a>
+                        <a href={data.link} id="linkedin"><Linkedin size="24" color="#ffffff"/></a>
+                        </div>
+                    </div> 
+                    <p className="Card-text">{data.desg}</p>
+                </div>
+                </div> )}  
+            </div>
+
             </div>
             
         </div>
