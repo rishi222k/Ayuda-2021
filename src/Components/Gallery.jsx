1fcf4e0d89537a934f42b205e7033d065a960575
rishi222k <rishiraghu22@yahoo.com>
2021-05-05 00:43:32 +0530

    Gallery Page

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Gallery = () => {
     return (
         <div className="Gallery">
         <h1>GALLERY</h1>
         <p>Every <span>Smile</span> has a story. <br/> With Each <span>Smile</span> we <span>capture,</span>  we hope to <br/><span> Connect, Inspire & Empower. </span></p>
-        <a href=".">Open Gallery</a>
+        <Link to="/gallery">Open Gallery</Link>
         <br/><br/><br/>
         </div>
     )
--- src/Pages/GalleryP.jsx
+++ src/Pages/GalleryP.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import logo from '../Images/Logohead.png'
 import {Link} from 'react-router-dom'
+import '../Styles/GalleryPage.scss'
 
 const GalleryP = () => {
     return (
         <div className="GalleryP">
         <div className="Ghead row">
-                <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 contoverflow">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5 contoverflow">
                     <Link to="/" className="title">
                         <img src={logo} alt="ayudalogo"/>
                         <p>Ayuda</p>
@@ -14,13 +15,13 @@ const GalleryP = () => {
                     <div className="content">
                     <h3>Gallery Images</h3>
                     {/* <h4>2016-2021</h4> */}
-                    <p>Every <span>Smile</span> has a story. <br/> With Each <span>Smile</span> we <span>capture,</span>  we hope to <br/><span> Connect, Inspire & Empower. </span>
+                    <p>Every <span>Smile</span> has a story. With Each <span>Smile</span> we <span>capture,</span>  we hope to <span> Connect, Inspire & Empower.</span>
                     </p>
                     <Link to="/">View Here</Link>
                     </div>
                 </div>
                 {/* <div className="col-0 col-sm-0 col-md-0 col-lg-1 col-xl-1"></div> */}
-                <div className="col-0 col-sm-0 col-md-0 col-lg-6 col-xl-6 galleryscroll">
+                <div className="col-0 col-sm-0 col-md-0 col-lg-7 col-xl-7 galleryscroll">
                     <div className="roll"></div>
                 </div>
             </div>
