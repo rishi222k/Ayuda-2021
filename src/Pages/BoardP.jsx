04973af39a58250d5b05a140d66cefa48537f422
rishi222k <rishiraghu22@yahoo.com>
2021-05-04 14:48:32 +0530

    fixes

--- src/Pages/BoardP.jsx
+++ src/Pages/BoardP.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../Images/Logohead.png'
 import '../Styles/BoardPage.scss'
 import {Link} from 'react-router-dom'
+import {Link as Scrolldown} from 'react-scroll'
 import Data from '../BoardData'
 import {Instagram} from '@styled-icons/boxicons-logos/Instagram'
 import { Linkedin } from '@styled-icons/boxicons-logos'
@@ -20,19 +21,19 @@ const BoardP = () => {
         <div className="BoardP">
              <div className="Bhead row">
                 <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 contoverflow">
-                    <div className="title">
+                    <Link to="/" className="title">
                         <img src={logo} alt="ayudalogo"/>
                         <p>Ayuda</p>
-                    </div>
+                    </Link>
                     <div className="content">
                     <h3>Board Members</h3>
                     <h4>2016-2021</h4>
                     <p>“It is so hard to say goodbye to <span> strangers</span> who turned into <span> your family.</span> We miss your presence in the NGO and promise to continue your <span> legacy with pride. </span>It’s been one blur of fun!”</p>
-                    <Link to="/">View Here</Link>
+                    <Scrolldown to="Bcards" smooth={true} duration={1000} >View Here</Scrolldown>
                     </div>
                 </div>
-                <div className="col-0 col-sm-0 col-md-0 col-lg-1 col-xl-1"></div>
-                <div className="col-0 col-sm-0 col-md-0 col-lg-5 col-xl-5 boardscroll">
+                {/* <div className="col-0 col-sm-0 col-md-0 col-lg-1 col-xl-1"></div> */}
+                <div className="col-0 col-sm-0 col-md-0 col-lg-6 col-xl-6 boardscroll">
                     <div className="roll"></div>
                 </div>
             </div>
--- src/Pages/GalleryP.jsx
+++ src/Pages/GalleryP.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import logo from '../Images/Logohead.png'
+import {Link} from 'react-router-dom'
 
 const GalleryP = () => {
     return (
         <div className="GalleryP">
-           
+        <div className="Ghead row">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 contoverflow">
+                    <Link to="/" className="title">
+                        <img src={logo} alt="ayudalogo"/>
+                        <p>Ayuda</p>
+                    </Link>
+                    <div className="content">
+                    <h3>Gallery Images</h3>
+                    {/* <h4>2016-2021</h4> */}
+                    <p>Every <span>Smile</span> has a story. <br/> With Each <span>Smile</span> we <span>capture,</span>  we hope to <br/><span> Connect, Inspire & Empower. </span>
+                    </p>
+                    <Link to="/">View Here</Link>
+                    </div>
+                </div>
+                {/* <div className="col-0 col-sm-0 col-md-0 col-lg-1 col-xl-1"></div> */}
+                <div className="col-0 col-sm-0 col-md-0 col-lg-6 col-xl-6 galleryscroll">
+                    <div className="roll"></div>
+                </div>
+            </div>
         </div>
     )
 }
