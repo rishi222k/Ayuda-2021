b18158d12071af1e44621e49408666dd252f219e
rishi222k <rishiraghu22@yahoo.com>
2021-05-05 05:39:22 +0530

    Gallery Page

---
+++ src/Gallerydata.js
@@ -0,0 +1,89 @@
+import pic1 from './Images/Gallery/galpic (1).jpeg'
+import pic2 from './Images/Gallery/galpic (2).jpg'
+import pic3 from './Images/Gallery/galpic (3).jpg'
+import pic4 from './Images/Gallery/galpic (4).jpg'
+import pic5 from './Images/Gallery/galpic (5).jpg'
+import pic6 from './Images/Gallery/galpic (6).jpg'
+import pic7 from './Images/Gallery/galpic (7).jpg'
+import pic8 from './Images/Gallery/galpic (8).jpg'
+import pic9 from './Images/Gallery/galpic (9).jpg'
+import pic10 from './Images/Gallery/galpic (10).jpg'
+import pic11 from './Images/Gallery/galpic (11).jpg'
+import pic12 from './Images/Gallery/galpic (12).jpg'
+import pic13 from './Images/Gallery/galpic (13).jpg'
+import pic14 from './Images/Gallery/galpic (14).jpg'
+import pic15 from './Images/Gallery/galpic (15).jpg'
+import pic16 from './Images/Gallery/galpic (16).jpg'
+import pic17 from './Images/Gallery/galpic (17).jpg'
+import pic18 from './Images/Gallery/galpic (18).jpg'
+import pic19 from './Images/Gallery/galpic (19).jpg'
+import pic20 from './Images/Gallery/galpic (20).jpg'
+import pic21 from './Images/Gallery/galpic (21).jpg'
+import pic22 from './Images/Gallery/galpic (22).jpg'
+import pic23 from './Images/Gallery/galpic (23).jpg'
+import pic24 from './Images/Gallery/galpic (24).jpg'
+import pic25 from './Images/Gallery/galpic (25).jpg'
+import pic26 from './Images/Gallery/galpic (26).jpg'
+import pic27 from './Images/Gallery/galpic (27).jpg'
+import pic28 from './Images/Gallery/galpic (28).jpg'
+import pic29 from './Images/Gallery/galpic (29).jpg'
+import pic30 from './Images/Gallery/galpic (30).jpg'
+import pic31 from './Images/Gallery/galpic (31).jpg'
+import pic32 from './Images/Gallery/galpic (32).jpg'
+import pic33 from './Images/Gallery/galpic (33).jpg'
+import pic34 from './Images/Gallery/galpic (34).jpg'
+import pic35 from './Images/Gallery/galpic (35).jpg'
+import pic36 from './Images/Gallery/galpic (36).jpg'
+import pic37 from './Images/Gallery/galpic (37).jpg'
+import pic38 from './Images/Gallery/galpic (38).jpg'
+import pic39 from './Images/Gallery/galpic (39).jpg'
+import pic40 from './Images/Gallery/galpic (40).jpg'
+
+
+
+const gdata=
+[
+    { id:"1", img:pic1 },
+    { id:"2", img:pic2 },
+    { id:"3", img:pic3 },
+    { id:"4", img:pic4 },
+    { id:"5", img:pic5 },
+    { id:"6", img:pic6 },
+    { id:"7", img:pic7 },
+    { id:"8", img:pic8 },
+    { id:"9", img:pic9 },
+    { id:"10", img:pic10 },
+    { id:"11", img:pic11 },
+    { id:"12", img:pic12 },
+    { id:"13", img:pic13 },
+    { id:"14", img:pic14 },
+    { id:"15", img:pic15 },
+    { id:"16", img:pic16 },
+    { id:"17", img:pic17 },
+    { id:"18", img:pic18 },
+    { id:"19", img:pic19 },
+    { id:"20", img:pic20 },
+    { id:"21", img:pic21 },
+    { id:"22", img:pic22 },
+    { id:"23", img:pic23 },
+    { id:"24", img:pic24 },
+    { id:"25", img:pic25 },
+    { id:"26", img:pic26 },
+    { id:"27", img:pic27 },
+    { id:"28", img:pic28 },
+    { id:"29", img:pic29 },
+    { id:"30", img:pic30 },
+    { id:"31", img:pic31 },
+    { id:"32", img:pic32 },
+    { id:"33", img:pic33 },
+    { id:"34", img:pic34 },
+    { id:"35", img:pic35 },
+    { id:"36", img:pic36 },
+    { id:"37", img:pic37 },
+    { id:"38", img:pic38 },
+    { id:"39", img:pic39 },
+    { id:"40", img:pic40 },
+    
+]
+
+export default gdata;
--- src/Pages/GalleryP.jsx
+++ src/Pages/GalleryP.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../Images/Logohead.png'
 import {Link} from 'react-router-dom'
+import Data from '../Gallerydata'
 import '../Styles/GalleryPage.scss'
 
 const GalleryP = () => {
@@ -14,20 +15,22 @@ const GalleryP = () => {
                     </Link>
                     <div className="content">
                     <h3>Gallery Images</h3>
-                    {/* <h4>2016-2021</h4> */}
                     <p>Every <span>Smile</span> has a story. With Each <span>Smile</span> we <span>capture,</span>  we hope to <span> Connect, Inspire & Empower.</span>
                     </p>
                     <Link to="/">View Here</Link>
                     </div>
                 </div>
-                {/* <div className="col-0 col-sm-0 col-md-0 col-lg-1 col-xl-1"></div> */}
                 <div className="col-0 col-sm-0 col-md-0 col-lg-7 col-xl-7 galleryscroll">
                     <div className="roll"></div>
                 </div>
             </div>
             <br/><br/><br/><br/><br/><br/>
             <div className="Gimages">
-                
+            {Data.map((data)=>
+            <div className="frame">
+                <img src={data.img} alt="Gallerypic" key={data.id}/>
+            </div>
+            )}
             </div>
         </div>
     )
