00f1c80f04051204385eab91071882aa3fdbb532
rishi222k <rishiraghu22@yahoo.com>
2021-05-07 02:52:02 +0530

    Initiative-Section

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -13,32 +13,51 @@ const Blogs = () => {
             <div className="card">
                 <h2>WHERE <span> HEALING</span> COMES FROM <span> HEART</span></h2>
                 <p>In this current situation, the world requires definite energy to shatter the negative forces around us. We all stay home and keeping ourselves away from the pandemic virus...</p>
-                <a href=".">Learn More</a>
+                <a 
+                href="https://ngoayuda.medium.com/where-healing-comes-from-heart-4991e82ea075"
+                target="_blank" 
+                rel="noopener noreferrer">
+                Learn More</a>
             </div>
             <div className="card">
                 <h2>THE <span> CHILDHOOD </span>DREAM</h2>
                 <p>Back in my school days, we had an annual visit to the old age home and an orphanage once a year at least. We’d go there and spend time with them, distribute things they might need or like and plan activities for them ... </p>
-                <a href=".">Learn More</a>
+                <a 
+                href="https://ngoayuda.medium.com/my-journey-in-ayuda-2-12845c62e256"
+                target="_blank" 
+                rel="noopener noreferrer">Learn More</a>
             </div>
             <div className="card">
                 <h2>THE PREDICAMENT OF <span> RISING POPULATION</span></h2>
                 <p>Every year, on July 11, World Population Day is observed by the UN with the primary narrative being to promulgate awareness on the challenges faced due to the ...</p>    
-                <a href=".">Learn More</a>
+                <a 
+                href="https://ngoayuda.medium.com/the-predicament-of-rising-population-97653858c8a"
+                target="_blank" 
+                rel="noopener noreferrer">Learn More</a>
             </div>
             <div className="card">
                 <h2><span> DON’T</span> GIVE UP - You are <span> important</span> as your <span>Life </span></h2>
                 <p>Every hour one student commits suicide in India, with about 28 such suicides reported every day, according to data compiled ...</p>
-                <a href=".">Learn More</a>
+                <a 
+                href="https://ngoayuda.medium.com/dont-give-up-you-are-important-as-your-life-dc3aa24027c2"
+                target="_blank" 
+                rel="noopener noreferrer">Learn More</a>
             </div>
             <div className="card">
                 <h2>MY JOURNEY IN <span>AYUDA</span></h2>
                 <p>When I look back at the two and a half years I have spent in this club, I see so many memories flooding my mind. I see the three Rivieras where we put our heart and souls to sell stuff, I see the countless number ...</p>
-                <a href=".">Learn More</a>
+                <a 
+                href="https://ngoayuda.medium.com/my-journey-in-ayuda-981d2b56ae9e"
+                target="_blank" 
+                rel="noopener noreferrer">Learn More</a>
             </div>
             <div className="card">
                 <h2>DRUG <span> ABUSE </span>AND ILLICIT <span>TRAFFICKING</span></h2>
                 <p>International Day Against Drug Abuse and Illicit Trafficking is a United Nations International Day against drug abuse and the illegal drug trade. It is observed annually on ...</p>
-                <a href=".">Learn More</a>
+                <a 
+                href="https://ngoayuda.medium.com/drug-abuse-and-illicit-trafficking-424d4e144c45"
+                target="_blank" 
+                rel="noopener noreferrer">Learn More</a>
             </div>
             
           
--- src/Components/Initiatives.jsx
+++ src/Components/Initiatives.jsx
@@ -1,6 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Carousel from "react-spring-3d-carousel";
+import { v4 as uuidv4 } from "uuid";
+import { config } from "react-spring";
+import sudhaar from '../Images/Initiatives/Sudhaar.png'
+import gyaan from '../Images/Initiatives/Gyaan.png'
+import nirmaan from '../Images/Initiatives/Nirmaan.png'
+import gogreen from '../Images/Initiatives/Go-Green.png'
+import muskaan from '../Images/Initiatives/Muskaan.png'
+import { ArrowLeftSquareFill, ArrowRightSquareFill } from '@styled-icons/bootstrap'
 
 const Initiatives = () => {
+    const [card, setCard] = useState({
+        goToSlide: 0,
+        offsetRadius: 2,
+        showNavigation: true,
+        config: config.slow
+      });
+
+    const InCards=[
+    {
+        key: uuidv4(),
+        content: <img src={sudhaar} alt="1" />,
+        link:"https://medium.com/series/ccf401c07ae"
+      },
+      {
+        key: uuidv4(),
+        content: <img src={gyaan} alt="2" />,
+        link:"https://medium.com/series/40893625143b"
+      },
+      {
+        key: uuidv4(),
+        content: <img src={nirmaan} alt="3" />,
+        link:"https://medium.com/series/fb1492a32180"
+      },
+      {
+        key: uuidv4(),
+        content: <img src={gogreen} alt="4" />,
+        link:"https://medium.com/series/46e8ab47fa44"
+      },
+      {
+        key: uuidv4(),
+        content: <img src={muskaan} alt="5" />,
+        link:"https://medium.com/series/48c090b2a22f"
+      },
+      {
+        key: uuidv4(),
+        content: <img src={sudhaar} alt="1" />,
+        link:"https://medium.com/series/ccf401c07ae"
+      },
+      {
+        key: uuidv4(),
+        content: <img src={gyaan} alt="2" />,
+        link:"https://medium.com/series/40893625143b"
+      },
+      {
+        key: uuidv4(),
+        content: <img src={nirmaan} alt="3" />,
+        link:"https://medium.com/series/fb1492a32180"
+      },
+      {
+        key: uuidv4(),
+        content: <img src={gogreen} alt="4" />,
+        link:"https://medium.com/series/46e8ab47fa44"
+      },
+      {
+        key: uuidv4(),
+        content: <img src={muskaan} alt="5" />,
+        link:"https://medium.com/series/48c090b2a22f"
+      },
+    ].map((slide,index)=>{
+        return {
+             ...slide,
+            onClick: () =>{
+                if(index===card.goToSlide)
+                window.open(slide.link);
+            setCard({ ...card, goToSlide: index })
+            }
+        }
+    });
+
+    const onRight =()=>{
+        setCard({  ...card, goToSlide: card.goToSlide + 1 })
+   }
+
+   const onLeft =()=>{
+       setCard({  ...card, goToSlide: card.goToSlide - 1 })
+   }
+
     return (
         <div className="Initiatives">
         <br/>
@@ -8,8 +94,26 @@ const Initiatives = () => {
         <div className="title">
         <h1><span>OUR</span> INITIATIVES</h1>
         </div>
+        <div className="initiative-cards">
+        <Carousel
+          slides={InCards}
+          goToSlide={card.goToSlide}
+          offsetRadius={card.offsetRadius}
+          showNavigation={card.showNavigation}
+          animationConfig={card.config}
+        />
+        </div>
         
-            
+        <div className="row arrows">
+        <ArrowLeftSquareFill 
+        size="40" 
+        onClick={onLeft} 
+        className="leftarr"/>
+        <ArrowRightSquareFill 
+        size="40" 
+        onClick={onRight}
+        className="rightarr"/>
+        </div>
         </div>
     )
 }
